2a33bd3c234df6f4415819ce358786e1545b774a
Charles Daoust <daoustcharles@yahoo.com>
2019-06-05 03:43:42 -0400

    added mysql

--- login.js
+++ login.js
@@ -1,3 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+
+//Create connections
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'K@Lender12',
+    database : 'jsSite'
+});
+
+//Connect to database
+db.connect((err)=>{
+  if(err){
+    throw err;
+  }
+  console.log('MySql Connected!')
+});
+const app = express();
+
+ app.listen('3000', ()=>{
+   console.log('Server started on port 3000');
+ });
 
 var objUsers = [
   {
@@ -16,7 +39,7 @@ function checkMe() {
   for(i = 0; i< objUsers.length;i++){
     if(username == objUsers[i].username && password == objUsers[i].password){
       console.log("correct")
-      window.location.href="index.html"; 
+      window.location.href="index.html";
       return
     }
   }alert("Incorrect password! try again")
