a9960739dd47b8f7dde8f841c95de322f87b13e2
Charles Daoust <daoustcharles@yahoo.com>
2019-06-05 03:46:39 -0400

    06/05/2019

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : 'K@Lender12',
+    password : '*****',
     database : 'jsSite'
 });
 
