b15935233f97df24c9d185bcc9533bef6daa6904
Charles Daoust <daoustcharles@yahoo.com>
2019-06-09 04:35:06 -0400

    connected to database and expiremented with loading data to the database

--- login.js
+++ login.js
@@ -5,8 +5,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : '*****',
-    database : 'jsSite'
+    password : 'password',
+    database: 'userpassdb'
 });
 
 //Connect to database
@@ -17,6 +17,35 @@ db.connect((err)=>{
   console.log('MySql Connected!')
 });
 const app = express();
+
+//Create db
+app.get('/createdb', (req, res)=> {
+  let sql = 'CREATE DATABASE userpassdb';
+  db.query(sql, (err , result) => {
+    if(err) throw err;
+    res.send('database created');
+  });
+});
+
+//Create tables
+app.get('/createusertable', (req, res)=>{
+  let sql = 'CREATE TABLE users(id int AUTO_INCREMENT, user VARCHAR(255), password VARCHAR(255), email VARCHAR(255), PRIMARY KEY(id))';
+  db.query(sql, (err, result)=>{
+    if(err) throw err;
+    res.send('table created')
+  })
+});
+
+//insert data into the user tables
+app.get('/adduser2', (req,res)=>{
+  let users = {user:'Vistor.Stone', password:'Cyborg',email:'detroit@hero.com'};
+  let sql = "INSERT INTO users SET ?";
+  let query = db.query(sql, users, (err, result)=>{
+    if(err) throw err;
+    console.log(result);
+    res.send('Data inserted into table');
+  });
+})
 
  app.listen('3000', ()=>{
    console.log('Server started on port 3000');
